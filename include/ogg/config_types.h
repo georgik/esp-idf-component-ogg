c2fb53859a65e74a1edc45a46833dfc43a8f7721
Juraj Michalek <juraj.michalek@gmail.com>
2024-10-13 06:10:26 +0200

    initial commit

---
+++ include/ogg/config_types.h
@@ -0,0 +1,16 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+/* Include standard integer types */
+#include <stdint.h>
+
+/* Define the ogg integer types using standard types */
+typedef int16_t  ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t  ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t  ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
+
+#endif /* __CONFIG_TYPES_H__ */
+
